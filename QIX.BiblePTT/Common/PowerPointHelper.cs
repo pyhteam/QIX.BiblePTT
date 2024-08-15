aef56b713b66a934fd22513aa5cb69067b4a942a
Ma Seo Sen <pyhteam.solutions@gmail.com>
2024-08-15 23:31:08 +0700

    Update new ver

--- QIX.BiblePTT/Common/PowerPointHelper.cs
+++ QIX.BiblePTT/Common/PowerPointHelper.cs
@@ -7,24 +7,32 @@ namespace QIX.BiblePTT.Common
     {
         public static void ExportPPT(string showPTTX)
         {
-            // Initialize Python Engine
-            Runtime.PythonDLL = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libs", "python310.dll");
-            PythonEngine.Initialize();
-
-            // Load Python script
-            using (Py.GIL())
+            try
             {
-                dynamic sys = Py.Import("sys");
-                sys.path.append(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libs"));
+                // Initialize Python Engine
+                Runtime.PythonDLL = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libs", "python310.dll");
+                PythonEngine.Initialize();
 
-                dynamic script = Py.Import("export_pptx");  // Tên file Python (không có phần mở rộng .py)
-                dynamic export_pptx = script.export_pptx;  // Lấy hàm export_pptx từ Python
-                // Gọi hàm Python từ C#
-                export_pptx(showPTTX);
+                // Load Python script
+                using (Py.GIL())
+                {
+                    dynamic sys = Py.Import("sys");
+                    sys.path.append(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "libs"));
+                    dynamic script = Py.Import("export_pptx");  // Tên file Python (không có phần mở rộng .py)
+                    dynamic export_pptx = script.export_pptx;  // Lấy hàm export_pptx từ Python 
+                    export_pptx(showPTTX);
+                }
             }
-
-            // Shutdown Python Engine
-            PythonEngine.Shutdown();
+            catch (Exception ex)
+            {
+                // Log or handle the exception as needed
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                // Shutdown Python Engine
+                PythonEngine.Shutdown();
+            }
         }
     }
 }
