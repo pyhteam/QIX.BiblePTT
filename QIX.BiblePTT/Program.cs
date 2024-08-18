402f7695c395eb4cca209b02a27fd382c39581f8
Ma Seo Sen <pyhteam.solutions@gmail.com>
2024-08-18 14:29:21 +0700

    update Add Viet and English ver

--- QIX.BiblePTT/Program.cs
+++ QIX.BiblePTT/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using QIX.BiblePTT.ControlViews;
 using QIX.BiblePTT.Services;
 using QIX.BiblePTT.Services.Interface;
@@ -15,32 +16,49 @@ namespace QIX.BiblePTT
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
 
-            var host = CreateHostBuilder().Build();
-            ServiceProvider = host.Services;
+                var host = CreateHostBuilder().Build();
+                ServiceProvider = host.Services;
 
-            Application.Run(ServiceProvider.GetRequiredService<fmMain>());
+                Application.Run(ServiceProvider.GetRequiredService<fmMain>());
+            }
+            catch (System.Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //write log here
+                // TODO: log error to a file or server
+                string fileName = "error.log";
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(fileName, true))
+                {
+                    sw.WriteLine($"{DateTime.Now.ToString()} - {ex.Message}");
+                }
+                Application.Exit();
+            }
+
         }
 
-        
+
 
         static IHostBuilder CreateHostBuilder()
         {
-           return Host.CreateDefaultBuilder()
-                .ConfigureServices((context, services) =>
-                {
-                    services.AddHttpClient(); // Register HttpClient
-                    services.AddTransient<IBibleService, BibleService>(); 
-                    services.AddTransient<IBookService, BookService>(); 
-                    services.AddTransient<IVerseService, VerseService>();
-                    services.AddTransient<IHymnBookService, HymnBookService>();
-                    services.AddSingleton<fmMain>();
-                    services.AddSingleton<BibleControlView>(); 
-                    services.AddSingleton<HymnsControlView>();
-                });
+            return Host.CreateDefaultBuilder()
+                 .ConfigureServices((context, services) =>
+                 {
+                     services.AddHttpClient(); // Register HttpClient
+                     services.AddTransient<IBibleService, BibleService>();
+                     services.AddTransient<IBookService, BookService>();
+                     services.AddTransient<IVerseService, VerseService>();
+                     services.AddTransient<IHymnBookService, HymnBookService>();
+                     services.AddSingleton<fmMain>();
+                     services.AddSingleton<BibleControlView>();
+                     services.AddSingleton<HymnsControlView>();
+                 });
         }
     }
 }
